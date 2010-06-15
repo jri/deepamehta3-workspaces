c2ade50553afc61e00f2578c0772bd66486c3f90
Jörg Richter <jri@deepamehta.de>
2010-06-15 19:45:41 +0200

    Bug Fix: search results are not assigned to any workspace (in order to not disturb workspace rendering).

--- src/main/java/de/deepamehta/plugins/workspaces/WorkspacesPlugin.java
+++ src/main/java/de/deepamehta/plugins/workspaces/WorkspacesPlugin.java
@@ -18,18 +18,18 @@ public class WorkspacesPlugin extends DeepaMehtaPlugin {
     @Override
     public void postCreateHook(Topic topic, Map<String, String> clientContext) {
         // Note 1: we do not relate search results to a workspace. Otherwise the search result would appear
-        // as relation when displaying the workspace. That's because an "Auxiliray" relation is not be
+        // as relation when displaying the workspace. That's because a "SEARCH_RESULT" relation is not be
         // created if there is another relation already.
         // Note 2: we do not relate workspaces to a workspace. This would be contra-intuitive.
-        if (!topic.typeId.equals("Serch Result") && !topic.typeId.equals("Workspace")) {
+        if (!topic.typeId.equals("Search Result") && !topic.typeId.equals("Workspace")) {
             String workspaceId = clientContext.get("workspace_id");
             if (workspaceId != null) {
                 dms.createRelation("RELATION", topic.id, Long.parseLong(workspaceId), null);
             } else {
-                logger.warning("Topic " + topic + " can't be related to a workspace (current workspace is unknown)");
+                logger.warning(topic + " can't be related to a workspace (current workspace is unknown)");
             }
         } else {
-            logger.info("### " + topic + " is not assigned to a workspace");
+            logger.info("### " + topic + " is deliberately not assigned to any workspace");
         }
     }
 
