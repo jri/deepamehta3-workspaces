520400da6372cf6ff8d76bea9725171e774cd0cd
Jörg Richter <jri@deepamehta.de>
2010-10-05 15:55:23 +0200

    Migration 2 fixed again: also the field renderers are properly restored.

--- src/main/java/de/deepamehta/plugins/workspaces/migrations/Migration2.java
+++ src/main/java/de/deepamehta/plugins/workspaces/migrations/Migration2.java
@@ -1,17 +1,19 @@
 package de.deepamehta.plugins.workspaces.migrations;
 
 import de.deepamehta.core.model.DataField;
-import de.deepamehta.core.model.Relation;
-import de.deepamehta.core.model.Topic;
 import de.deepamehta.core.model.TopicType;
 import de.deepamehta.core.service.Migration;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Logger;
 
 
 
+/**
+ * Repairs to data field definitions which were broken in deepamehta3-workspaces v0.4.
+ * <p>
+ * Note: this migration is distributed with deepamehta3-workspaces v0.4.1. and is set to run only when updating
+ * from deepamehta3-workspaces v0.4 to v0.4.1 (see migration2.properties).
+ */
 public class Migration2 extends Migration {
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
@@ -25,7 +27,9 @@ public class Migration2 extends Migration {
         TopicType workspaceType = dms.getTopicType("de/deepamehta/core/topictype/Workspace");
         DataField nameField        = workspaceType.getDataField("de/deepamehta/core/property/Name");
         DataField descriptionField = workspaceType.getDataField("de/deepamehta/core/property/Description");
+        nameField.setRendererClass("TitleRenderer");
         nameField.setIndexingMode("FULLTEXT");
+        descriptionField.setRendererClass("BodyTextRenderer");
         descriptionField.setIndexingMode("FULLTEXT");
     }
 }
